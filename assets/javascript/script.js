2940c98caa187b5260f675a6f314fc973b63a390
Dark Knight <jordanccall@gmail.com>
2020-04-06 11:43:49 -0600

    added index, stylesheets, script; also added firebase information

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBTMDOPn3ku44SC5YWvyD7QbxQ-xz-l9ZE",
+    authDomain: "train-schedule-9point75.firebaseapp.com",
+    databaseURL: "https://train-schedule-9point75.firebaseio.com",
+    projectId: "train-schedule-9point75",
+    storageBucket: "train-schedule-9point75.appspot.com",
+    messagingSenderId: "891863033066",
+    appId: "1:891863033066:web:7c93c9124580183be72393"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
